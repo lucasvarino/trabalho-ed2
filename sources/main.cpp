0913c6faa2a660ae0a138e8efe33dcf809bfe9a6
Lucas Varino <lucasvarino@gmail.com>
2022-10-10 13:36:35 -0300

    Função de Print do ProductReview feita

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
+#include "ProductReview.h"
+
+using namespace std;
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+
+    ProductReview* prod1 = new ProductReview("1", "1", 5.0, "10/10/2022");
+    prod1->print();
 }
