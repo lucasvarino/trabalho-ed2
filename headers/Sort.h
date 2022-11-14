f13939c43119a537762e05dad04847d14b4a7186
Pedro Grimaldi <pedroghansel@gmail.com>
2022-11-13 21:36:33 -0300

    Codigo comentado

--- headers/Sort.h
+++ headers/Sort.h
@@ -8,12 +8,30 @@
 
 using namespace std;
 
+// Classe responsável pelos metodos de ordenação
 class Sort
 {
     private:
+        // Variáveis
+        int comparacoes;
+        int trocas;
+        double tempo;
+
+        // Funções Auxiliares
         void merge(ProductReview *vetor, int inicio, int meio, int fim);
         
     public:
+        // Funções Get
+        int getComparacoes() {return comparacoes;};     
+        int getTrocas() {return trocas;};
+        double getTempo() {return tempo;};
+
+        // Funções Set
+        void setComparacoes(int comparacoes){this->comparacoes = comparacoes;};
+        void setTrocas(int trocas) {this->trocas = trocas;};
+        void setTempo(double tempo) {this->tempo = tempo;};   
+        
+        //Funções de Ordenação
         void mergeSort(ProductReview *vetor, int inicio, int fim);
         
 };
